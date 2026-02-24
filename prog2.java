3267f4dfa2391722b29b521be934e42848dd6b61
toadof <gautampl444@gmail.com>
2026-02-24 23:04:20 +0530

    2nd question

---
+++ prog2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class prog2 {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		if(a>b)
+			if(a>c)
+				System.out.print(a);
+			else
+				System.out.print(c);
+		else if(b>c)
+			System.out.print(b);
+		else
+			System.out.print(c);
+
+
+	}
+}
