8c6a32a8d8440130ebb5864a9431dcbe90e45461
toadof <gautampl444@gmail.com>
2026-02-24 23:26:01 +0530

    question 3

---
+++ prog3.java
@@ -0,0 +1,23 @@
+public class prog3 {
+	public static void main(String[] args){
+		int a=1;
+		int b=2;
+		int c=3;
+		int d=4;
+		int e=5;
+		int op=4;
+		switch(op){
+		case 1:System.out.print(a);
+			break;
+		case 2:System.out.print(b);
+			break;
+		case 3:System.out.print(c);
+			break;
+		case 4:System.out.print(d);
+			break;
+		case 5:System.out.print(e);
+			break;
+		default:break;
+		}
+	}
+}
