e23e16267159cd5dcd2bbb1184bb17024b1922d3
toadof <gautampl444@gmail.com>
2026-02-25 19:01:08 +0530

    7 ques

--- prog7.java
+++ prog7.java
@@ -2,12 +2,17 @@ import java.util.*;
 public class prog7 {
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
+		int a=6;
 		for(int i=0;i<a;i++){
-			for(int j=0;j<=i;j++){
-				System.out.print("*");
+			for(int j=a;j>=i;j--){
+				System.out.print(" ");
 			}
+			for(int k=0;k<=i;k++)
+				System.out.print("*");
+			for(int x=1;x<=i;x++)
+				System.out.print("*");
 			System.out.println(" ");
+
 			
 		}
 	}
