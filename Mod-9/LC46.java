321da8a198d4be17dfed907086f4c6c46a4fa6b7
toadof <gautampl444@gmail.com>
2026-03-17 23:58:39 +0530

    3 more

--- Mod-9/LC46.java
+++ Mod-9/LC46.java
@@ -1,1 +1,25 @@
 //LeetCode 46 – Permutations (Medium)
+class Solution {
+    public void elements(int[] arr,List<Integer> cur,List<List<Integer>> res,boolean[] used){
+        int n=arr.length;
+        if(cur.size()==n){res.add(new ArrayList<>(cur));return;}
+        for(int i=0;i<n;i++){
+            if(!used[i]){
+                cur.add(arr[i]);
+                used[i]=true;
+                elements(arr,cur,res,used);
+                cur.remove(cur.size()-1);
+                used[i]=false;
+            }
+        }
+
+   
+    }
+    public List<List<Integer>> permute(int[] nums) {
+         List<List<Integer>> res = new ArrayList<>();
+    boolean[] used = new boolean[nums.length];
+    elements(nums, new ArrayList<>(), res, used);
+    return res;
+
+    }
+}
--- Mod-9/LC78.java
+++ Mod-9/LC78.java
@@ -1,1 +1,16 @@
 //LeetCode 78 – Subsets (Medium)
+class Solution {
+    static void subset(int[] arr,int index,List<Integer> current,List<List<Integer>> list){
+		if(index==arr.length){
+        list.add(new ArrayList<>(current));return;}
+		current.add(arr[index]);
+		subset(arr,index+1,current,list);
+		current.remove(current.size()-1);
+		subset(arr,index+1,current,list);
+	}
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list=new ArrayList<>();
+          subset(nums,0,new ArrayList<>(),list);
+        return list;
+    }
+}
